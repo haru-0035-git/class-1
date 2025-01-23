a3e576e9ff523f04b7b767188ae8ae744ad11e92
moteki-0035-git <motekiruna.9684@gmail.com>
2025-01-23 11:42:53 +0900

    13章まで来た

--- class-flask/20250116/models.py
+++ class-flask/20250116/models.py
@@ -1,8 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
-# ▼▼▼ リスト 11-1の追加 ▼▼▼
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
-# ▲▲▲ リスト 11-1の追加 ▲▲▲
+from sqlalchemy.orm import relationship
 
 # Flask-SQLAlchemyの生成
 db = SQLAlchemy()
@@ -11,8 +10,6 @@ db = SQLAlchemy()
 # モデル
 # ==================================================
 # メモ
-
-
 class Memo(db.Model):
     # テーブル名
     __tablename__ = 'memos'
@@ -22,26 +19,34 @@ class Memo(db.Model):
     title = db.Column(db.String(50), nullable=False)
     # 内容
     content = db.Column(db.Text)
+    # ▼▼▼ リスト13-1追加 ▼▼▼
+    # ユーザーID
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id', name="fk_memos_users"), nullable=False)
 
-# ▼▼▼ リスト 11-1の追加 ▼▼▼
+    # User とのリレーション
+    user = relationship("User", back_populates = "memos")
+    # ▲▲▲ リスト13-1追加 ▲▲▲
+    
 # ユーザー
-
-
 class User(UserMixin, db.Model):
     # テーブル名
     __tablename__ = 'users'
-    # ID（PK）
+    # ID（PK）    
     id = db.Column(db.Integer, primary_key=True)
     # ユーザー名
     username = db.Column(db.String(50), unique=True, nullable=False)
     # パスワード
     password = db.Column(db.String(120), nullable=False)
+    
+    # ▼▼▼ リスト13-1追加 ▼▼▼
+    # Memo とのリレーション
+    # リレーション: １対多
+    memos = relationship("Memo", back_populates = "user")
+    # ▲▲▲ リスト13-1追加 ▲▲▲
+    
     # パスワードをハッシュ化して設定する
-
     def set_password(self, password):
         self.password = generate_password_hash(password)
     # 入力したパスワードとハッシュ化されたパスワードの比較
-
     def check_password(self, password):
         return check_password_hash(self.password, password)
-# ▲▲▲ リスト 11-1の追加 ▲▲▲
