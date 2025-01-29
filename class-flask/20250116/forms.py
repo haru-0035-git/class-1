74390bd87e49c60d0dc4c43b4c9e86d5081f56b4
moteki-0035-git <motekiruna.9684@gmail.com>
2025-01-29 15:38:06 +0900

    授業

--- class-flask/20250116/forms.py
+++ class-flask/20250116/forms.py
@@ -9,12 +9,10 @@ from models import Memo, User
 # Formクラス
 # ==================================================
 # メモ用入力クラス
-
-
 class MemoForm(FlaskForm):
     # タイトル
-    title = StringField('タイトル：', validators=[DataRequired('タイトルは必須入力です'),
-                                             Length(max=10, message='10文字以下で入力してください')])
+    title = StringField('タイトル：', validators=[DataRequired('タイトルは必須入力です'), 
+                            Length(max=10, message='10文字以下で入力してください')])
     # 内容
     content = TextAreaField('内容：')
     # ボタン
@@ -31,29 +29,25 @@ class MemoForm(FlaskForm):
 
 # ▼▼▼ リスト 11-2の追加 ▼▼▼
 # ログイン用入力クラス
-
-
 class LoginForm(FlaskForm):
-    username = StringField('ユーザー名：',
+    username = StringField('ユーザー名：', 
                            validators=[DataRequired('ユーザー名は必須入力です')])
     # パスワード：パスワード入力
     password = PasswordField('パスワード: ',
                              validators=[Length(4, 10,
-                                                'パスワードの長さは4文字以上10文字以内です')])
+                                    'パスワードの長さは4文字以上10文字以内です')])
     # ボタン
     submit = SubmitField('ログイン')
-
+    
     # カスタムバリデータ
     # 英数字と記号が含まれているかチェックする
     def validate_password(self, password):
-        if not (any(c.isalpha() for c in password.data) and
-                any(c.isdigit() for c in password.data) and
-                any(c in '!@#$%^&*()' for c in password.data)):
+        if not (any(c.isalpha() for c in password.data) and \
+            any(c.isdigit() for c in password.data) and \
+            any(c in '!@#$%^&*()' for c in password.data)):
             raise ValidationError('パスワードには【英数字と記号：!@#$%^&*()】を含める必要があります')
 
 # サインアップ用入力クラス
-
-
 class SignUpForm(LoginForm):
     # ボタン
     submit = SubmitField('サインアップ')
@@ -64,3 +58,12 @@ class SignUpForm(LoginForm):
         if user:
             raise ValidationError('そのユーザー名は既に使用されています')
 # ▲▲▲ リスト 11-2の追加 ▲▲▲
+
+# ▼▼▼ リスト 14-6の追加 ▼▼▼
+# Wiki用入力クラス
+class WikiForm(FlaskForm):
+    # タイトル
+    keyword = StringField('検索ワード：', render_kw={"placeholder": "入力してください"})
+    # ボタン
+    submit = SubmitField('Wiki検索')
+# ▲▲▲ リスト 14-6の追加 ▲▲▲
