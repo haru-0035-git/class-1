66341d6af61ac8544b5e2e2e1456c53d70052e6f
moteki-0035-git <148021294+moteki-0035-git@users.noreply.github.com>
2025-02-10 15:10:39 +0900

    Add files via upload

---
+++ list/list/listApp.swift
@@ -0,0 +1,17 @@
+//
+//  listApp.swift
+//  list
+//
+//  Created by classmac on 2025/02/10.
+//
+
+import SwiftUI
+
+@main
+struct listApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
